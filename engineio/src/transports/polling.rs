eb8b7d8e0f8659b3f7cd0b76921cc0340ffc0d77
ssebo <ssebo.zzz@gmail.com>
2022-09-10 21:54:42 +0800

    chore: polling transport add test

--- engineio/src/transports/polling.rs
+++ engineio/src/transports/polling.rs
@@ -33,8 +33,6 @@ pub struct ServerPolling {
     receiver: Arc<Mutex<Receiver<Bytes>>>,
 }
 
-// impl PollingTransport {}
-
 impl ClientPolling {
     pub fn new(mut url: Url, headers: Option<HeaderMap>) -> Result<Self> {
         let mut builder = ClientBuilder::new();
@@ -51,6 +49,11 @@ impl ClientPolling {
         }
 
         Ok(Self { client, url })
+    }
+
+    #[cfg(test)]
+    pub(crate) fn url(&self) -> Url {
+        self.url.clone()
     }
 }
 
@@ -135,5 +138,53 @@ impl Stream for ServerPolling {
             Some(bytes) => Poll::Ready(Some(Ok(bytes))),
             None => Poll::Ready(None),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use tokio::sync::mpsc::channel;
+
+    use super::*;
+    use futures_util::StreamExt;
+    use std::str::FromStr;
+
+    #[test]
+    fn polling_transport_url() -> Result<()> {
+        let url = Url::from_str("http://127.0.0.1").unwrap();
+        let transport = ClientPolling::new(url.clone(), None).unwrap();
+        assert_eq!(
+            transport.url().to_string(),
+            url.to_string() + "?transport=polling"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_server_polling_transport() -> Result<()> {
+        let (send_tx, mut send_rx) = channel(100);
+        let (recv_tx, recv_rx) = channel(100);
+        let mut transport = ServerPolling::new(send_tx, recv_rx);
+
+        let data = Bytes::from_static(b"1Hello\x1e1HelloWorld");
+
+        recv_tx.send(data.clone()).await.unwrap();
+
+        let msg = transport.next().await;
+        assert!(msg.is_some());
+        let msg = msg.unwrap();
+        assert!(msg.is_ok());
+        let msg = msg.unwrap();
+
+        assert_eq!(msg, data);
+
+        let payload = Payload::String(data.clone());
+        transport.emit(payload).await?;
+        let msg = send_rx.recv().await;
+        assert!(msg.is_some());
+        let msg = msg.unwrap();
+        assert_eq!(msg, data);
+
+        Ok(())
     }
 }
